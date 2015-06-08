1f104d462a2d4906d0d77c72ae0851d807b558f7
Ken Gullaksen <ken.gullaksen@embriq.no>
2015-06-08 20:24:55 +0200

    small refactoring. use same gmap search to populate query param for route. Also prepare for metadata mapping

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,38 +1,43 @@
 export default Ember.Controller.extend({
   search: null,
+  gmapsMeta: null,
   searching: false,
   queryParams: ['q'],
   placeholder: 'f.eks: Oslo',
   isGeoEnabled: window.navigator.geolocation,
 
+  _queryGmapsApi: function (queryObj) {
+    var controller = this;
+    Ember.$.getJSON(
+      'https://maps.googleapis.com/maps/api/geocode/json',
+      queryObj,
+      function (data) {
+        controller.set('searching', false);
+        controller.set('gmapsMeta', data.results[0]); // TODO build object from results { city:x, kommune:x, region:x }
+        controller.transitionToRoute({queryParams: {q: data.results[0].formatted_address}});
+      }
+    );
+  },
   actions: {
     query: function () {
       var controller = this,
-          query = controller.get('search');
-      console.log("query: %s", query);
+        query = controller.get('search');
       if (query) {
-        // TODO use googleapis to get match
-        controller.transitionToRoute({queryParams: {q: query}});
+        controller.set('searching', true);
+        // TODO: perhaps implement multiselect based on gmaps results for address
+        controller._queryGmapsApi({address: query});
       }
     },
     geoQuery: function () {
       var controller = this;
-      console.log("geoquery");
+      if (!controller.get('isGeoEnabled')) {
+        return;
+      }
+
+      controller.set('search', "");
+      controller.set('searching', true);
       navigator.geolocation.getCurrentPosition(function (pos) {
-        controller.set('searching', true);
-        Ember.$.getJSON(
-          'https://maps.googleapis.com/maps/api/geocode/json',
-          {
-            latlng: '' + pos.coords.latitude + ',' + pos.coords.longitude
-          },
-          function (data) {
-            var town = data.results.filter(function (r) {
-              return r.types[0] === 'postal_town';
-            });
-            controller.set('searching', false);
-            controller.transitionToRoute({queryParams: {q: town[0].formatted_address}});
-          }
-        );
+        controller._queryGmapsApi({latlng: '' + pos.coords.latitude + ',' + pos.coords.longitude});
       });
     }
   }
